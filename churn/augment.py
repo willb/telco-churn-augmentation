dcdf12a81bb82ba5e908522dbb7292237742e56a
William Benton <willb@nvidia.com>
2021-03-18 10:54:30 -0500

    cast now to date in data augmentation

--- churn/augment.py
+++ churn/augment.py
@@ -190,7 +190,7 @@ def billing_events(df):
             ).alias("value"),
             F.when(df.Churn == "Yes", get_last_month(df.customerID)).otherwise(0).alias("last_month")
         )
-        .withColumn("now", F.lit(now))
+        .withColumn("now", F.lit(now).cast("date"))
         .withColumn("month_number", -(F.row_number().over(w) + F.col("last_month")))
         .withColumn("date", F.expr("add_months(now, month_number)"))
         .drop("now", "month_number", "last_month")
